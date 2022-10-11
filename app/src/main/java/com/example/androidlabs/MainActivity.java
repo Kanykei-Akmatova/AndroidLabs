a63abbead4c711c28df08315e7f0d0fd2cc42448
Kanykei Akmatova <kanykeyakmatova@gmail.com>
2022-10-10 21:48:09 -0400

    Lab 4 - Refactoring

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -33,9 +33,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText editText = (EditText) findViewById(R.id.editText);
-        Button button = (Button) findViewById(R.id.buttonAdd);
-        Switch switchUrgent = (Switch) findViewById(R.id.switchUrgent);
+        EditText editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.buttonAdd);
+        Switch switchUrgent = findViewById(R.id.switchUrgent);
 
         button.setOnClickListener((click) -> {
             ToDoItem toDoItem = new ToDoItem(editText.getText().toString(), switchUrgent.isChecked());
@@ -44,72 +44,20 @@ public class MainActivity extends AppCompatActivity {
             adapter.notifyDataSetChanged();
         });
 
-        ListView listView = (ListView) findViewById(R.id.myListView);
-        listView.setAdapter(adapter = new ToDoItemListAdapter());
+        ListView listView = findViewById(R.id.myListView);
+        listView.setAdapter(adapter = new ToDoItemListAdapter(this, toDoItemList));
         listView.setOnItemClickListener((parent, view, position, id) -> {
             AlertDialog.Builder alertDiBuilder = new AlertDialog.Builder(this);
             alertDiBuilder.setTitle(R.string.alert_title)
-                    .setMessage(getString(R.string.alert_message) + position)
-                    .setPositiveButton(R.string.alert_yes, (click, arg) -> {
-                        Toast.makeText(getApplicationContext(),"Index " + position,
-                        Toast.LENGTH_SHORT).show();
+                    .setMessage(getString(R.string.alert_message) + (position + 1))
+                    .setPositiveButton(R.string.alert_yes, (dialog, arg) -> {
                         toDoItemList.remove(position);
                         adapter.notifyDataSetChanged();
+                        dialog.dismiss();
                     })
-                    .setNegativeButton(R.string.alert_no, (click, arg) -> {
-
+                    .setNegativeButton(R.string.alert_no, (dialog, arg) -> {
                     });
             alertDiBuilder.create().show();
         });
-    }
-
-    private class ToDoItemListAdapter extends BaseAdapter {
-        private Context context;
-        private ArrayList<ToDoItem> items;
-
-        public ToDoItemListAdapter() {
-        }
-
-        public ToDoItemListAdapter(Context context, ArrayList<ToDoItem> items) {
-            this.context = context;
-            this.items = items;
-        }
-
-        @Override
-        public int getCount() {
-            return toDoItemList.size();
-        }
-
-        @Override
-        public Object getItem(int index) {
-            return toDoItemList.get(index);
-        }
-
-        @Override
-        public long getItemId(int index) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int index, View oldView, ViewGroup parent) {
-            View newView = oldView;
-            LayoutInflater inflater = getLayoutInflater();
-
-            if (newView == null) {
-                newView = inflater.inflate(R.layout.row_layout, parent, false);
-            }
-
-            ToDoItem toDoItem = (ToDoItem) getItem(index);;
-
-            TextView textView = newView.findViewById(R.id.testViewRow);
-            textView.setText(toDoItem.getToDo());
-
-            if(toDoItem.getUrgent()) {
-                textView.setTextColor(Color.WHITE);
-                textView.setBackgroundColor(Color.RED);
-            }
-
-            return newView;
-        }
     }
 }
