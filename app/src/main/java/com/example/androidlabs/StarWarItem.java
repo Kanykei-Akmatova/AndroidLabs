4d86471a7ecd95d216025c9ff6416ae6755af1e0
Kanykei Akmatova <kanykeyakmatova@gmail.com>
2022-10-29 00:45:12 -0400

    Lab 7 - Setting Activities

---
+++ app/src/main/java/com/example/androidlabs/StarWarItem.java
@@ -0,0 +1,16 @@
+package com.example.androidlabs;
+
+public class StarWarItem {
+    private String name = "";
+    private int height = 0;
+    private int mass = 0;
+
+    public StarWarItem(){
+    }
+
+    public StarWarItem(String name, int height, int mass){
+        this.name = name;
+        this.height = height;
+        this.mass = mass;
+    }
+}
